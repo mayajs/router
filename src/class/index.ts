db83617540394eedfa9dd8d369a6a4306b0046f8
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-12 23:05:46 +0800

    FEAT: Add class folder

---
+++ src/class/index.ts
@@ -0,0 +1,37 @@
+import { MayaJsContext, ModuleWithProviders } from "../interface";
+import { ControllerMiddleware, ControllerType, ModuleCustomType, ModuleImports, ModuleProviders, ParentModule } from "../types";
+
+export abstract class Services {}
+export class RoutesMapper {}
+
+export abstract class MayaJsModule {
+  declarations: ControllerType[] = [];
+  imports: ModuleImports[] = [];
+  exports: (ModuleCustomType | ControllerType)[] = [];
+  providers: ModuleProviders = [];
+  dependencies: any[] = [];
+  parent: ParentModule = null;
+  path = "";
+}
+
+/**
+ * An abstract class that define all the methods for a single route
+ */
+export class MayaJsController {
+  middlewares: Partial<ControllerMiddleware> = {};
+  routes: any[] = [];
+  GET(ctx: MayaJsContext): Promise<any> | any {}
+  POST(ctx: MayaJsContext): Promise<any> | any {}
+  DELETE(ctx: MayaJsContext): Promise<any> | any {}
+  PUT(ctx: MayaJsContext): Promise<any> | any {}
+  PATCH(ctx: MayaJsContext): Promise<any> | any {}
+  OPTIONS(ctx: MayaJsContext): Promise<any> | any {}
+  HEAD(ctx: MayaJsContext): Promise<any> | any {}
+}
+
+export abstract class CustomModule extends MayaJsModule {
+  invoke() {}
+  static forRoot(...args: any): ModuleWithProviders {
+    return { module: class extends CustomModule {}, providers: [] };
+  }
+}
