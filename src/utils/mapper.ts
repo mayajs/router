ef7fae4fa8668c2974e6c29d92413a78ca36672e
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-15 04:19:41 +0800

    FEAT: Remove routeMapper function

--- src/utils/mapper.ts
+++ src/utils/mapper.ts
@@ -1,9 +1,9 @@
-import { ModuleCustomType, ParentModule, ModuleMapper, ModuleMapperFactory, RouterMapper, RouterMapperFactory } from "../types";
+import { ModuleCustomType, ParentModule, ModuleMapper, ModuleMapperFactory } from "../types";
 import { ModuleWithProviders, ModuleWithProvidersProps } from "../interface";
-import { dependencyMapperFactory, logger, sanitizePath } from "./helpers";
+import { dependencyMapperFactory } from "./helpers";
 import { CustomModule } from "../class";
 
-const mapModules: ModuleMapperFactory = (router, app, parentModule = null): ModuleMapper => (imported) => {
+export const mapModules: ModuleMapperFactory = (router, parentModule = null): ModuleMapper => (imported) => {
   let args: any[] = [];
   let customModuleProps: ModuleWithProvidersProps = { dependencies: [], providers: [], imports: [] };
   let currentModule;
@@ -27,20 +27,18 @@ const mapModules: ModuleMapperFactory = (router, app, parentModule = null): Modu
     tempModule.providers = providers;
     tempModule.dependencies = dependencies ?? [];
     tempModule.imports = imports ?? [];
-    args = mapDependencies(app.dependencies, tempModule);
+    args = mapDependencies(router.dependencies, tempModule);
   }
-
-  if (currentModule.name === "RouterModule") args[1] = (parentModule as any)?.parent?.path ?? "";
 
   const _module = new currentModule(...args);
 
   if (parentModule) _module.parent = parentModule as CustomModule;
   if (isCustomModule) (_module as CustomModule).invoke();
 
-  _module.imports.map(mapModules(router, app, _module));
+  _module.imports.map(mapModules(router, _module));
 };
 
-const declarationsMapper = (_module: ParentModule, name: string = ""): boolean => {
+export const declarationsMapper = (_module: ParentModule, name: string = ""): boolean => {
   let isDeclared = _module?.declarations !== undefined;
 
   if (_module && _module?.declarations) isDeclared = _module.declarations.some((item) => item.name === name);
@@ -49,58 +47,3 @@ const declarationsMapper = (_module: ParentModule, name: string = ""): boolean =
 
   return isDeclared;
 };
-
-const routeMapper: RouterMapperFactory = (router, app, _module = null): RouterMapper => {
-  // Recursive function for mapping array of routes
-  const RouterMapper: RouterMapper = (parent = "") => (route) => {
-    // Create parent route
-    parent = parent.length > 0 ? sanitizePath(parent) : "";
-
-    // Sanitize route path
-    route.path = parent + sanitizePath(route.path);
-
-    if (_module !== null) _module.path = route.path;
-
-    const controllerName = route?.controller?.name;
-    let isDeclared = true;
-
-    if (controllerName && _module !== null) {
-      isDeclared = declarationsMapper(_module, controllerName);
-    }
-
-    if (!isDeclared) {
-      const moduleName = _module?.constructor.name;
-      throw new Error(`${controllerName} is not declared in ${moduleName}`);
-    }
-
-    // Add route to list
-    router.addRouteToList(route, _module);
-
-    if (route?.children !== undefined && route?.loadChildren !== undefined) {
-      logger.red(`Property 'loadChildren' can't be used with 'children' in route '${route.path}'`);
-      throw new Error();
-    }
-
-    if (route?.controller !== undefined && route?.loadChildren !== undefined) {
-      logger.red(`Property 'loadChildren' can't be used with 'controller' in route '${route.path}'`);
-      throw new Error();
-    }
-
-    // Check if route has children
-    if (route?.children && route?.children.length > 0) {
-      // Map children's routes
-      route.children.map(RouterMapper(route.path));
-    }
-
-    if (route?.loadChildren) {
-      route
-        .loadChildren()
-        .then(mapModules(router, app, _module ?? { path: route.path }))
-        .catch((error) => console.log(error));
-    }
-  };
-
-  return RouterMapper;
-};
-
-export default routeMapper;
