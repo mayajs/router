ac27e31975b6682f6ab59839f9fcf3a199aa5862
Mac Ignacio <mack_ignacio@yahoo.com>
2021-04-15 06:52:33 +0800

    Fix: Mapping of dependency trees

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { RouterDependencies, RouterProps, Type } from "../interface";
-import { ModuleProviders, ParentModule } from "../types";
+import { ParentModule } from "../types";
+import { Services } from "../class";
 
 // We use '+' instead of template string '${}' because of performance gain
 // See https://stackoverflow.com/questions/6094117/prepend-text-to-beginning-of-string
@@ -29,8 +30,8 @@ export const logger = {
   red: (value: string) => console.log(`\x1b[31m${value}\x1b[0m`),
 };
 
-const mapProviders = (name: string, _module?: ParentModule): undefined | ModuleProviders | Function => {
-  if (!_module) return;
+const mapProviders = (name: string, _module?: ParentModule): undefined | Type<Services> => {
+  if (!_module || !_module?.providers) return;
   const index = _module?.providers?.findIndex((item) => item.name === name);
   return index > -1 ? _module?.providers[index] : mapProviders(name, _module.parent);
 };
@@ -38,27 +39,35 @@ const mapProviders = (name: string, _module?: ParentModule): undefined | ModuleP
 const findDependency = (name: string, dependencies: RouterDependencies, props: RouterProps, _module?: ParentModule) => {
   if (dependencies[name]) return dependencies[name];
 
-  const provider = mapProviders(name, _module) as Type<ModuleProviders>;
+  const provider = mapProviders(name, _module) as any;
 
-  if (provider) {
-    dependencies[name] = new provider();
-    return dependencies[name];
-  }
+  if (!provider || !provider?.__injectable__) return;
+
+  let args: any[] = [];
+
+  if (provider.dependencies.length > 0) args = mapDependencies(dependencies, _module, provider.dependencies);
+
+  const providerInstance: Services = new provider(...args);
+  dependencies[name] = providerInstance;
+  return providerInstance;
 };
 
 export function mapDependencies(routerDep: RouterDependencies, _module?: ParentModule, dependencies?: any[]) {
   const props = getFunctionProps<RouterProps>(mapDependencies);
   const _dependencies = dependencies ?? _module?.dependencies;
   const promitives = ["String", "Boolean", "Function", "Array"];
-  return _dependencies
-    ? _dependencies.map(({ name }) => {
-        if (promitives.includes(name)) return undefined;
-        const dependency = findDependency(name, routerDep, props, _module);
-        if (!dependency) {
-          logger.red(`${name} is not provided properly in a module.`);
-          throw new Error();
-        }
-        return dependency;
-      })
-    : [];
+
+  const mapDependencyItems = ({ name }: any) => {
+    if (promitives.includes(name)) return undefined;
+
+    const dependency = findDependency(name, routerDep, props, _module);
+
+    if (!dependency) {
+      logger.red(`${name} is not provided properly in a module.`);
+      throw new Error();
+    }
+    return dependency;
+  };
+
+  return _dependencies ? _dependencies.map(mapDependencyItems) : [];
 }
