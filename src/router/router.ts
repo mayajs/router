d97b5c12aaea3cb9b3329bd7412f4c5cf9ddfc1c
Mac <mack_ignacio@yahoo.com>
2021-12-26 22:42:01 +0800

    Rename function to router

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,82 +1,205 @@
-import { MayaJsContext, MayaRouter, RouterContext, RouterProps, VisitedRoutes } from "../interface";
-import { ResponseSender } from "../types";
-import middleware from "./middleware";
-import functions from "./functions";
+import { RouteMethod, RouterMethods, RouterProps, MayaJsRoute, RouteBody, Route } from "../interface";
+import { routeFinderFactory, logger, mapDependencies, sanitizePath } from "../utils/helpers";
+import { RequestMethod, RouteCallback, RouterFunction } from "../types";
+import merge from "../utils/merge";
+import regex from "../utils/regex";
+import { props } from "./app";
+import { declarationsMapper, mapModules } from "../utils/mapper";
 
-export const props: RouterProps = {
-  routes: { "": { middlewares: [] } as any },
-  visitedRoutes: {},
-  middlewares: [],
-  context: {},
-  dependencies: {},
-};
-
-const app: MayaRouter = {
-  init: () => {},
-  use: (middleware) => app,
-  add: (routes) => {},
-  headers: { "X-Powered-By": "MayaJS" },
-  router: functions(props),
+// Export default route object
+const router: RouterMethods = {
+  addRouteToList: (route, _module) => {},
+  findRoute: (path, method) => null,
+  executeRoute: (path, route) => Promise.resolve(),
+  visitedRoute: (path, method) => null,
+  mapper: (path, method) => (route) => {},
   ...props,
 };
 
-app.add = function (routes) {
-  // Check if routes is an array
-  if (!Array.isArray(routes)) return this.router.addRouteToList(routes);
+router.addRouteToList = function (route, _module) {
+  // Get the parent path
+  const parent = _module?.parent ? _module?.parent.path : "";
 
-  // Map routes and add each route to the list
-  routes.map(this.router.mapper());
-};
+  // Sanitize current route path
+  const path = (parent + route.path).replace(/^\/+|\/+$/g, "");
 
-app.init = function () {
-  // Initialize mayajs router
-  this.router = functions({ ...app });
-};
+  const createCommonRoute = (path: string[], routes: RouteBody, key: RequestMethod, options: MayaJsRoute, parent: Route): any => {
+    const current = path[0];
 
-app.use = function (middleware) {
-  // Add middleware to the list
-  if (middleware) this.router.middlewares.push(middleware);
+    if (current === "") return (this.routes[""][key] = options);
+    if (!routes?.[current]) routes[current] = { middlewares: [...(parent.middlewares ?? []), ...(parent.guards ?? [])] } as any;
+    if (routes[current] && path.length === 1) return ((routes[current] as RouteBody)[key] = options);
 
-  return this;
-};
+    path.shift();
+    return createCommonRoute(path, routes[current] as RouteBody, key, options, parent);
+  };
 
-// Sends a reponse message and ending the request
-const send: ResponseSender = async (context: RouterContext) => {
-  // Get method, path and res in context object
-  const { method, path, res } = context;
+  // List of request method name
+  const methods = ["GET", "POST", "PUT", "HEAD", "DELETE", "OPTIONS", "PATCH"];
 
-  // Check if path exist in visited routes or in non-param routes
-  const route = app.router.visitedRoute(path, method) || app.router.findRoute(path, method);
+  if (route.path === "") route?.middlewares?.map((item) => (this.routes[""] as RouteBody).middlewares.push(item));
 
-  if (!route) {
-    // Route was not found. Send back an error message
-    return res.send({ message: `${method}: '${path}' was not found!` }, 404);
+  if (route.controller && route.hasOwnProperty("controller")) {
+    const dependencies = mapDependencies(this.dependencies, _module, route?.dependencies || (route.controller as any).dependencies);
+    const controller = new route.controller(...dependencies);
+    const controllerProps = Object.getOwnPropertyNames(Object.getPrototypeOf(controller)) as RequestMethod[];
+    const routes = (controller as any)["routes"];
+
+    routes.map(({ middlewares, methodName, path: routePath, requestMethod }: any) => {
+      // Create callback function
+      const callback = (args: any) => (controller as any)[methodName](args) as RouteCallback;
+
+      // Create parent route
+      const parent = path === "" ? "/" : path;
+
+      routePath = routePath.startsWith("/") ? routePath : `/${routePath}`;
+      middlewares = [...this.routes[""].middlewares, ...this.middlewares, ...(route?.middlewares ?? []), ...(route?.guards ?? []), ...middlewares];
+
+      // Add controller route to list
+      this.addRouteToList({ path: sanitizePath(parent + routePath), middlewares, [requestMethod]: callback });
+    });
+
+    controllerProps.map((key: RequestMethod) => {
+      if (methods.includes(key)) {
+        let middlewares = controller?.middlewares?.[key] ?? [];
+        let guards = controller?.guards?.[key] ?? [];
+
+        middlewares = [...this.routes[""].middlewares, ...this.middlewares, ...(route?.middlewares ?? []), ...guards, ...middlewares];
+
+        // Create callback function
+        const callback = (args: any) => controller[key](args) as RouteCallback;
+
+        const options = { middlewares, dependencies: [], method: key, regex: regex(path), callback, path };
+
+        createCommonRoute(path.split("/"), this.routes[""], key, options, route);
+      }
+    });
   }
+
+  if (!route?.controller) {
+    (Object.keys(route) as RequestMethod[]).map((key): void => {
+      if (methods.includes(key) && route.hasOwnProperty("controller")) {
+        throw new Error(`Property controller can't be used with ${key} method on route '${path}'`);
+      }
+
+      // Check if key is a method
+      if (methods.includes(key) && !route.hasOwnProperty("controller")) {
+        // Get current method
+        const current = route[key] as RouteMethod;
+
+        // Set default middlewares from route
+        let middlewares = route?.middlewares ?? [];
+
+        let guards = route?.guards ?? [];
+
+        // Check if current method has middlewares
+        if (current?.middlewares) {
+          middlewares = [...middlewares, ...current.middlewares];
+        }
+
+        const routeCallback = (args: any) => (route[key] as RouteCallback)(args);
+
+        // Create callback function
+        const callback = current?.callback ?? routeCallback;
+
+        const options = {
+          middlewares: [...this.routes[""].middlewares, ...guards, ...middlewares],
+          dependencies: [],
+          method: key,
+          regex: regex(path),
+          callback,
+          path,
+        };
+
+        createCommonRoute(path.split("/"), this.routes[""], key, options, route);
+      }
+    });
+  }
+};
+
+router.findRoute = function (path, method) {
+  return path !== "" ? routeFinderFactory(path)(path.split("/"), this.routes[""], method, []) : this.routes[""][method];
+};
+
+router.executeRoute = async function (path, route) {
+  // Set message variable
+  let message = "";
 
   try {
-    Object.keys(app.headers).map((key) => res.setHeader(key, app.headers[key]));
+    const context = this.context;
+    // Try to execute route callback
+    message = await route.callback(context);
 
-    // Create MayaJS params
-    const params = (route as VisitedRoutes).params || { ...route.regex.exec(path)?.groups };
+    if (!this.visitedRoutes[path]) {
+      // Initialize path for caching
+      this.visitedRoutes[path] = {} as any;
+    }
 
-    // Create MayaJS context
-    app.router.context = { ...context, params };
-
-    // Create a factory method for executing current route
-    const execute = async (context: MayaJsContext) => {
-      app.router.context = context;
-      res.send(await app.router.executeRoute(path, route));
-    };
-
-    const middlewares = route.middlewares !== undefined ? route.middlewares : [];
-
-    // Run middlewares before calling the main route callback
-    middleware([...app.router.middlewares, ...middlewares], app.router.context, execute);
+    if (!this.visitedRoutes[path][route.method]) {
+      // Cache path route if not on visited routes
+      this.visitedRoutes[path][route.method] = { ...route, ...context };
+    }
   } catch (error) {
-    // Send error back to client
-    res.send(error);
+    // Catch error when running callback
+    message = error;
   }
+
+  return message;
 };
 
-// Export app instance and send function
-export { app, send };
+router.visitedRoute = function (path, method) {
+  return this?.visitedRoutes && this?.visitedRoutes[path] && this?.visitedRoutes[path][method] ? this?.visitedRoutes[path][method] : null;
+};
+
+router.mapper = function (parent = "", _module = null) {
+  const _this = this;
+
+  return (route) => {
+    // Create parent route
+    parent = parent.length > 0 ? sanitizePath(parent) : "";
+
+    // Sanitize route path
+    route.path = parent + sanitizePath(route.path);
+
+    if (_module !== null) _module.path = route.path;
+
+    const controllerName = route?.controller?.name;
+    let isDeclared = true;
+
+    // Check if controller is declared on a module
+    if (controllerName && _module !== null) isDeclared = declarationsMapper(_module, controllerName);
+
+    // Throw error if controller is not declared in a module
+    if (!isDeclared) {
+      const moduleName = _module?.constructor.name;
+      logger.red(`${controllerName} is not declared in ${moduleName}`);
+      throw new Error();
+    }
+
+    // Add route to list
+    _this.addRouteToList(route, _module);
+
+    if (route?.children !== undefined && route?.loadChildren !== undefined) {
+      logger.red(`Property 'loadChildren' can't be used with 'children' in route '${route.path}'`);
+      throw new Error();
+    }
+
+    if (route?.controller !== undefined && route?.loadChildren !== undefined) {
+      logger.red(`Property 'loadChildren' can't be used with 'controller' in route '${route.path}'`);
+      throw new Error();
+    }
+
+    // Check if route has children
+    if (route?.children && route?.children.length > 0) route.children.map(_this.mapper(route.path, _module));
+
+    // Load all children asynchronously
+    if (route?.loadChildren) {
+      route
+        .loadChildren()
+        .then(mapModules(_this, _module ?? { path: route.path }))
+        .catch((error) => console.log(error));
+    }
+  };
+};
+
+export default (app: RouterProps): RouterFunction => merge(app, router);
