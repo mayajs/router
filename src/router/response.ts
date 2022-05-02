5d58c68a31b9cc80457e7fbfba7c1779c714f3df
Mac <mack_ignacio@yahoo.com>
2022-05-03 00:33:32 +0800

    Set response to html when value is passed html pattern

--- src/router/response.ts
+++ src/router/response.ts
@@ -20,7 +20,7 @@ function ResponseFunctions(res: http.ServerResponse): MayaJsResponse {
       const htmlPattern = /<([^>]+?)([^>]*?)>(.*?)<\/\1>/gi;
       try {
         if (!isText) response = this.json(value?.message && isError ? { message: value?.message } : value, code);
-      }
+        else if (htmlPattern.test(value)) response = this.html(value, code);
       } catch (error) {
       }
 
