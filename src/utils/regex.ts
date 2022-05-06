02d56f3ea470d03f7fe65eae968d1755021f26b4
Mac <mack_ignacio@yahoo.com>
2022-05-06 10:57:47 +0800

    Add ability to set whether to match the full string

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,7 +1,7 @@
-function regex(url: string) {
-  const base = (val: string) => new RegExp(`^${val}$`);
-  const notParam = (val: string) => `(?:\\/?(${val}))`;
-  const isParam = (val: string) => `(?:\\/?(?<${val}>[\\w\\-]+?))`;
+function regex(url: string, full = false) {
+  const base = (val: string) => new RegExp(`^${val}${full ? "$" : ""}`);
+  const notParam = (val: string) => `(?:\\/(${val}))`;
+  const isParam = (val: string) => `(?:\\/(?<${val}>[\\w\\-]+?))`;
   const pattern = url
     .split("/")
     .map((val) => (val.includes(":") ? isParam(val.substring(1)) : notParam(val)))
