8e35d55aa9edaeca6bb5ee5aabbb206369b34d03
Mac <mack_ignacio@yahoo.com>
2022-05-12 17:23:02 +0800

    Add resolveDir function

--- src/router/static.ts
+++ src/router/static.ts
@@ -2,3 +2,5 @@ import { MIME_TYPES } from "../utils/constants";
 import { RouterContext } from "../interface";
 import { promises as fs } from "fs";
 import path from "path";
+
+const resolveDir = (filepath: string) => path.resolve(process.cwd(), "./public" + filepath);
